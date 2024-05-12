bac01bf3a1daf3d48652fdfbd421dcb9e14e215b
hantv <hantv12a3@gmail.com>
2024-05-12 07:41:47 +0700

    fix: display weather condition

--- src/modules/telegram/telegram.service.ts
+++ src/modules/telegram/telegram.service.ts
@@ -1,22 +1,32 @@
+// Nest dependencies
 import { Injectable, Logger } from '@nestjs/common';
-import axios from 'axios';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { WeatherService } from '../weather/weather.service';
+
+// Other dependencies
+import axios from 'axios';
 import * as dayjs from 'dayjs';
+
+// Local dependencies
+import { WeatherService } from '../weather/weather.service';
 
 @Injectable()
 export class TelegramService {
-  constructor(private readonly weatherService: WeatherService) {};
+  constructor(private readonly weatherService: WeatherService) {}
 
-  async sendMessageToChannel(message: any, photo) {
+  async sendMessageToChannel(message: any) {
     const urlSendChannelTele = `${process.env.TELEGRAM_ORG_KEY}/${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
-    
-    await axios.get(urlSendChannelTele,
-      { params: { chat_id: process.env.TELEGRAM_CHAT_ID, text: message, parse_mode: "HTML", photo} });
+
+    await axios.get(urlSendChannelTele, {
+      params: {
+        chat_id: process.env.TELEGRAM_CHAT_ID,
+        text: message,
+        parse_mode: 'HTML',
+      },
+    });
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_6AM)
-  async handleCronEvery10Seconds() {
+  async handleCronEveryDayAt6Am() {
     const { data } = await this.weatherService.getWeatherCity();
 
     const dataFormat = {
@@ -24,19 +34,17 @@ export class TelegramService {
       clouds: data.clouds,
       wind: data.wind,
       main: data.main,
-      weather: data.weather
-    }
-
-    const urlImageWeather = `https://openweathermap.org/img/wn/${dataFormat.weather.icon}@2x.png`;
+      weather: data.weather,
+    };
 
     const message = `\nThời tiết: ${dataFormat.name}
     \nNgày ${dayjs(new Date()).get('days')} tháng ${dayjs(new Date()).get('months')} năm ${dayjs(new Date()).get('years')}
     \nNhiệt độ: ${dataFormat.main.temp}°C
-    \nThời tiết: ${dataFormat.weather.description}
+    \nThời tiết: ${dataFormat.weather[0].description}
     \nNhiệt độ ngoài trời: ${dataFormat.main.feels_like}°C
     \nĐộ ẩm: ${dataFormat.main.humidity}%
     \nTốc độ gió: ${dataFormat.wind.speed}km/h`;
 
-    await this.sendMessageToChannel(message, urlImageWeather);
+    await this.sendMessageToChannel(message);
   }
 }
