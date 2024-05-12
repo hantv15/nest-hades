44d0270c4f67d22e84401b91cd3a7221beab32d9
hantv <hantv12a3@gmail.com>
2024-05-12 08:48:32 +0700

    fix: format date weather information

--- src/modules/weather/weather.service.ts
+++ src/modules/weather/weather.service.ts
@@ -7,11 +7,13 @@ export class WeatherService {
   private readonly logger = new Logger(WeatherService.name);
 
   async getWeatherCity() {
+    const city: string = "Hà Nội";
+
     return await axios({
       method: 'GET',
       url: process.env.API_OPEN_WEATHER_MAP_URL,
       params: {
-        q: "Hà Nội",
+        q: city,
         units: "metric",
         appid: process.env.API_OPEN_WEATHER_MAP_KEY,
         lang: 'vi'
